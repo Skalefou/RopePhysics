1e800dc59569d85dc030d067a3d294830e757359
Skalefou <45128700+Skalefou@users.noreply.github.com>
2020-11-24 22:44:59 +0100

    add click mouse IN PROGRESS

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,44 @@
 constexpr unsigned int ropeSize = 800;
 constexpr unsigned int windowHeight = 400;
 
+signed int CoordsHeight(sf::RenderWindow &window) {
+    sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+    return localPosition.y - (windowHeight / 2);
+}
+
+bool isMouseLeftReleased() {
+    static bool click = false;
+    if (click == true && !(sf::Mouse::isButtonPressed(sf::Mouse::Left))) {
+        click = false;
+        return true;
+    }
+    else if (click == false && sf::Mouse::isButtonPressed(sf::Mouse::Left))
+        click = true;
+    return false;
+}
+
+unsigned int positiveNumberConvert(signed int number) {
+    if (number < 0)
+        number *= -1;
+    return number;
+}
+
+void clickRelease(sf::RenderWindow& window, signed int ropePositionY[]) {
+    if (isMouseLeftReleased()) {
+        signed int mouseHeight = CoordsHeight(window);
+        if (sf::Mouse::getPosition(window).x < ropeSize && sf::Mouse::getPosition(window).x >= 0) {
+            ropePositionY[sf::Mouse::getPosition(window).x] = CoordsHeight(window);
+            for (unsigned int i = 0; i < positiveNumberConvert(CoordsHeight(window)); i++) {
+
+            }
+        }
+    }
+}
+
 int main(void) {
     sf::RenderWindow window(sf::VideoMode(ropeSize, windowHeight), "Rope Physics", sf::Style::Close);
     sf::VertexArray rope(sf::Points, ropeSize);
+    signed int ropePositionY[ropeSize] = { 0 };
 
     for (unsigned int i = 0; i < ropeSize; i++) {
         rope[i].position = sf::Vector2f((float)i, (float)(windowHeight / 2));
@@ -30,6 +65,8 @@ int main(void) {
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
             window.close();
+
+        clickRelease(window, ropePositionY);
 
         window.draw(rope);
         window.display();
